2285d75093a11a46184720e18cbb853e153d0fb5
Alice Wong <alicew1313@gmail.com>
2021-12-16 17:56:36 -0800

    new content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Project1 from "./pages/Project1";
+import Home from "./pages/Home";
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/project1">About</Link>
+            </li>
+          </ul>
+
+          <hr />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/project1" element={<Project1 />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Project1.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Project1 extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                <h2>About Page</h2>
+                <main>
+                    <p>This section contains information about...</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+
+
+export default Project1;
