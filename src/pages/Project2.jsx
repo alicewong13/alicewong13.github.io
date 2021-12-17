e2a4fe18f09600d92a4ef328897f7d55ac7cf250
alicewong13 <64674429+alicewong13@users.noreply.github.com>
2021-12-17 12:32:19 -0800

    Delete src directory

--- src/pages/Project2.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Project2() {
-    return (
-        <div>Project2</div>
-    );
-}
-
-export default Project2;
--- src/pages/Project3.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Project3() {
-  return <div>Project3</div>
-}
-
-export default Project3
